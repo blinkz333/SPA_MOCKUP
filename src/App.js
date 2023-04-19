f11faf6e6e350459a6120765c4015a61daa1e5da
blink <threerapon.rat@gmail.com>
2023-04-19 23:17:01 +0700

    fix route

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Desktop from "./pages/Desktop";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Desktop />} />
+      <Route path="/frontend_responsive_mockup" element={<Desktop />} />
     </Routes>
   );
 }
